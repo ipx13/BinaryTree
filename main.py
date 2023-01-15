b48d21d4c44140029f568863f453c1b485466ee5
XXairship <116432782+ipx13@users.noreply.github.com>
2023-01-15 11:00:55 +0800

    Added a conditional that puts the element in either of left or right node

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ class BinarySearchTreeNode:
     def add_child(self, data):
         if data == self.data:
             return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
