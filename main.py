da7dff6344aefa2d1189ca00a3bc3ba5558cd442
XXairship <116432782+ipx13@users.noreply.github.com>
2023-01-15 11:21:53 +0800

    Added a search function that looks for a certain element within the list

--- main.py
+++ main.py
@@ -19,6 +19,22 @@ class BinarySearchTreeNode:
             else:
                 self.right = BinarySearchTreeNode(data)
 
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
     def in_order_traversal(self):
         elements = []
         if self.left:
